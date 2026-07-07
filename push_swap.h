e051be9e5b2e1253501d8488e1665216e669ffd2
olSampaio <lucassampaiofelipe@gmail.com>
2026-07-07 12:57:01 -0300

    feat: adiciona função swap
    unifica funções sa e sb em uma função com o mesmo comportamento, e função swap é criada. A função ss permanece, mudança no arquivo header para adaptação das mudanças.

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lusampai <lusampai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 17:38:29 by lusampai          #+#    #+#             */
-/*   Updated: 2026/07/06 12:40:48 by lusampai         ###   ########.fr       */
+/*   Updated: 2026/07/07 12:49:21 by lusampai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,21 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+// DECLARAÇÃO DO STRUCT
+
+typedef struct s_stack
+{
+    int             value;
+    int             index;
+    struct s_stack  *next;
+    struct s_stack  *prev;
+}   t_stack;
+
 // FUNÇÕES DE OPERAÇÕES PUSH_SWAP
 
-void	sa(t_stack *list);
-void	sb(t_stack *list);
-void	ss(t_stack *list);
-void	pa(t_stack **list_a, t_stack **list_b);
-void	pb(t_stack **list_a, t_stack **list_b);
+void	ft_swap(t_stack **list);
+void	ss(t_stack **list_a, t_stack **list_b);
+void	ft_push(t_stack **src, t_stack **dest);
 void	ra(t_stack *list);
 void	ra(t_stack *list);
 void	rr(t_stack *list);
@@ -33,13 +41,6 @@ void	rrr(t_stack *list);
 // FUNÇÕES AUXILIARES
 
 void	ft_lstadd_front(t_stack **lst, t_stack *new);
-
-typedef struct s_stack
-{
-    int             value;
-    int             index;
-    struct s_stack  *next;
-    struct s_stack  *prev;
-}   t_stack;
+t_stack	*ft_lstlast(t_stack *lst);
 
 #endif
