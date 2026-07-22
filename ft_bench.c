bf156c693335d068504aae9c7af148d932cac590
Lucas Sampaio Felipe <lusampai@c3r5p2.42sp.org.br>
2026-07-22 18:35:16 -0300

    feat:extrai bench para arquivo próprio

---
+++ ft_bench.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bench.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lusampai <lusampai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/22 18:33:47 by lusampai          #+#    #+#             */
+/*   Updated: 2026/07/22 18:34:09 by lusampai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_bench(char *algorithm_name, int disorder, t_operations *ops)
+{	
+	int total_ops;
+
+	total_ops = (ops->sa + ops->pb + ops->rra +
+		ops->rrb + ops->rrr + ops->ra + ops->rb + ops->rr + ops->sa +
+			ops->sb + ops->ss + ops->pa);
+	ft_printf("[bench] disorder:	%d.%d%%\n", disorder / 100, disorder % 100);	
+	ft_printf("[bench] strategy:	%s\n", algorithm_name);
+	ft_printf("[bench] total_ops:	%d\n", total_ops);
+	ft_printf("[bench] sa: %d sb: %d ss: %d pa: %d pb: %d\n",
+		ops->sa, ops->sb, ops->ss, ops->pa, ops->pb);
+	ft_printf("[bench] ra: %d rb: %d rr: %d rra: %d rrb: %d rrr: %d",
+		ops->ra, ops->rb, ops->rr, ops->rra, ops->rrb, ops->rrr);
+}
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lusampai <lusampai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 19:18:37 by lusampai          #+#    #+#             */
-/*   Updated: 2026/07/22 18:12:37 by lusampai         ###   ########.fr       */
+/*   Updated: 2026/07/22 18:34:46 by lusampai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,22 +94,6 @@ void	fillstruct(t_operations *ops)
 	ops->sb = 0;
 	ops->ss = 0;
 	ops->pa = 0;
-}
-
-static void	ft_bench(char *algorithm_name, int disorder, t_operations *ops)
-{	
-	int total_ops;
-
-	total_ops = (ops->sa + ops->pb + ops->rra +
-		ops->rrb + ops->rrr + ops->ra + ops->rb + ops->rr + ops->sa +
-			ops->sb + ops->ss + ops->pa);
-	ft_printf("[bench] disorder:	%d.%d%%\n", disorder / 100, disorder % 100);	
-	ft_printf("[bench] strategy:	%s\n", algorithm_name);
-	ft_printf("[bench] total_ops:	%d\n", total_ops);
-	ft_printf("[bench] sa: %d sb: %d ss: %d pa: %d pb: %d\n",
-		ops->sa, ops->sb, ops->ss, ops->pa, ops->pb);
-	ft_printf("[bench] ra: %d rb: %d rr: %d rra: %d rrb: %d rrr: %d",
-		ops->ra, ops->rb, ops->rr, ops->rra, ops->rrb, ops->rrr);
 }
 
 int	main(int argc, char **argv)
