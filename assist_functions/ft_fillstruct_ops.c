5d9ff6e7cbff32e12b90b29f7cce9d3ea4a9ff37
Lucas Sampaio Felipe <lusampai@c1r8p3.42sp.org.br>
2026-07-23 12:32:38 -0300

    refactor:extrai funções para assist_functions
    retira funções do arquivo main e as move para seus próprios arquivos na pasta assist_functions

---
+++ assist_functions/ft_fillstruct_ops.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fillstruct_ops.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lusampai <lusampai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/23 12:22:08 by lusampai          #+#    #+#             */
+/*   Updated: 2026/07/23 12:22:21 by lusampai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	ft_fillstruct_ops(t_operations *ops)
+{
+	ops->pb = 0;
+	ops->rra = 0;
+	ops->rrb = 0;
+	ops->rrr = 0;
+	ops->ra = 0;
+	ops->rb = 0;
+	ops->rr = 0;
+	ops->sa = 0;
+	ops->sb = 0;
+	ops->ss = 0;
+	ops->pa = 0;
+}
