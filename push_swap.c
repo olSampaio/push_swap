9579c2304b7cf8d885d889aeeb2553ad5ba798a4
armarque <Arthur Marques Barbosa Silva armarque@student.42sp.org.br>
2026-07-23 12:32:40 -0300

    calcula qual algoritmo usar baseado no lstsize também

--- push_swap.c
+++ push_swap.c
@@ -3,36 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lusampai <lusampai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: armarque <armarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 19:18:37 by lusampai          #+#    #+#             */
-/*   Updated: 2026/07/23 12:14:52 by lusampai         ###   ########.fr       */
+/*   Updated: 2026/07/23 12:30:13 by armarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/ft_printf.h"
 #include "push_swap.h"
-#include "ft_printf/ft_printf.h"
 
 static void	ft_build_list(t_stack **list_a, char **argv, int argc)
 {
 	int		i;
 	t_stack	*new_node;
-	int		j;
+	long	value;
 
 	i = argc - 1;
 	while (i >= 1)
 	{
-		j = 0;
 		if (ft_isnumber(argv[i]) == 1)
 		{
+			value = ft_atoi(argv[i]);
+			if (value > INT_MAX || value < INT_MIN)
+				ft_error(list_a);
+			if (ft_is_repeated(*list_a, value))
+				return (ft_error(list_a));
 			new_node = malloc(sizeof(t_stack));
-			new_node->value = atoi(argv[i]);
+			new_node->value = (int)value;
 			new_node->index = 0;
 			new_node->next = NULL;
 			new_node->prev = NULL;
 			ft_lstadd_front(list_a, new_node);
-		}		
+		}
+		else
+			ft_error(list_a);
 		i--;
 	}
 }
@@ -41,14 +46,14 @@ static int	verify_flags(char **argv, int *use_bench)
 {
 	int	i;
 	int	algorithm;
-	
+
 	algorithm = 0;
 	i = 1;
 	if (ft_strcmp(argv[1], "--bench") == 0)
 	{
 		*use_bench = 1;
-		i = 2;		
-	}	
+		i = 2;
+	}
 	if (ft_strcmp(argv[i], "--simple") == 0)
 		algorithm = 1;
 	else if (ft_strcmp(argv[i], "--medium") == 0)
@@ -62,7 +67,10 @@ static int	verify_flags(char **argv, int *use_bench)
 
 static char	*ft_call_algorithm(t_stack **list_a, t_stack **list_b,
 		int algorithm_choice, int disorder, t_operations *ops)
-{		
+{
+	int	sizelist;
+
+	sizelist = ft_lstsize(*list_a);
 	if (algorithm_choice == 1)
 		return (ft_selection_sort(list_a, list_b, ops), "Simple / O(n²)");
 	else if (algorithm_choice == 2)
@@ -71,12 +79,12 @@ static char	*ft_call_algorithm(t_stack **list_a, t_stack **list_b,
 		return (ft_radix_sort(list_a, list_b, ops), "Complex / O(n log n)");
 	else if (algorithm_choice == 0)
 	{
-		if (disorder < 20)
+		if ((sizelist <= 3) || (disorder < 20))
 			return (ft_selection_sort(list_a, list_b, ops), "Adaptive / O(n²)");
-		else if (20 <= disorder && disorder <= 50)
+		else if ((sizelist <= 5) || (20 <= disorder && disorder <= 50))
 			return (ft_bucket_sort(list_a, list_b, ops), "Adaptive / O(n√n)");
 		else
-			return (ft_radix_sort(list_a, list_b, ops), "Adaptive / O(n log n)");
+			return (ft_radix_sort(list_a, list_b, ops), "Adaptive/ O(n log n)");
 	}
 	return (0);
 }
@@ -115,12 +123,16 @@ int	main(int argc, char **argv)
 		printf("Uso: %s <numeros>\n", argv[0]);
 		return (1);
 	}
-	algorithm_choice = verify_flags(argv, &use_bench);	
+	algorithm_choice = verify_flags(argv, &use_bench);
+	ft_printf("algorithm = %d\n", algorithm_choice);
 	ft_build_list(&list_a, argv, argc);
 	ft_set_index(list_a);
+	if (ft_issorted(list_a))
+		return (ft_exit(&list_a, &list_b));
 	disorder = compute_disorder(list_a);
-	algorithm_name = ft_call_algorithm(&list_a, &list_b, algorithm_choice, disorder / 100, &ops);
-	if(use_bench == 1)
+	algorithm_name = ft_call_algorithm(&list_a, &list_b, algorithm_choice,
+			disorder / 100, &ops);
+	if (use_bench == 1)
 		ft_bench(algorithm_name, disorder, &ops);
-	return (0);
+	return (ft_exit(&list_a, &list_b));
 }
